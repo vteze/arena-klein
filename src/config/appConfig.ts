0fa9445f2ed78d5624f360c262380e95665631d9
Victor Cruz <vitinhozcruz@gmail.com>
2025-05-30 15:14:58 +0000

    Adicione os horários das 07:00 e 08:00 da manhã nas reservas

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -23,6 +23,7 @@ export const courts: Court[] = [
 ];
 
 export const availableTimeSlots: string[] = [
+  '07:00', '08:00', // Horários adicionados
   '09:00', '10:00', '11:00', '12:00', '13:00', '14:00',
   '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'
 ];
